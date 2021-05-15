1966f7ccd8082900f61a7914bff28f1806b475c8
Polina <schachowa.paulina@gmail.com>
2021-05-15 21:07:59 +0300

    fix tests

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -23,24 +23,28 @@ func TestBalancer(t *testing.T) {
 			trafficCnt: 0,
 		},
 	}
+
+	up = []bool{true, true, false}
 	mockedServersPool[0].trafficCnt = 50
 	mockedServersPool[1].trafficCnt = 20
 	// ----
 	serve := min(mockedServersPool, func(s1, s2 server) bool { return s1.trafficCnt < s2.trafficCnt })
 
-	assert.Equal(mockedServersPool[serve].dst, mockedServersPool[2].dst)
-	// ----
-	mockedServersPool[2].trafficCnt = 40
-	serve = min(mockedServersPool, func(s1, s2 server) bool { return s1.trafficCnt < s2.trafficCnt })
-	// Now server with index "1" has least traffic
-
 	assert.Equal(mockedServersPool[serve].dst, mockedServersPool[1].dst)
 	// ----
-	mockedServersPool[1].trafficCnt = 70
+	mockedServersPool[1].trafficCnt = 60
 	serve = min(mockedServersPool, func(s1, s2 server) bool { return s1.trafficCnt < s2.trafficCnt })
-	// Now server with least traffic is down, so server with least traffic that is healthy is with index "0"
+	// Now server  "1" has least traffic
 
 	assert.Equal(mockedServersPool[serve].dst, mockedServersPool[0].dst)
+	// ----
+
+	mockedServersPool[1].trafficCnt = 70
+
+	up[2] = true
+	serve = min(mockedServersPool, func(s1, s2 server) bool { return s1.trafficCnt < s2.trafficCnt })
+	// Now server with least traffic is up, so server with least traffic that is healthy is "3"
+	assert.Equal(mockedServersPool[serve].dst, mockedServersPool[2].dst)
 }
 
 func TestBalancerError(t *testing.T) {
--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"testing"
@@ -9,18 +10,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const baseAddress = "http://balancer:8090"
+//const baseAddress = "http://localhost:8090"
+var target = flag.String("target", "http://localhost:8090", "request target")
 
 var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
 func TestBalancer(t *testing.T) {
+	flag.Parse()
 	var server string
 	for i := 0; i < 10; i++ {
-		url := fmt.Sprintf("%s/api/v1/some-data", baseAddress)
-		t.Log(fmt.Sprintf("Sending request to %s", url))
-		resp, err := client.Get(url)
+		//		url := fmt.Sprintf("%s/api/v1/some-data", baseAddress)
+		t.Log(fmt.Sprintf("Sending request to %s", *target))
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", *target))
+
 		if err != nil {
 			t.Error(err)
 		}
@@ -39,8 +43,9 @@ func TestBalancer(t *testing.T) {
 }
 
 func BenchmarkBalancer(b *testing.B) {
+	flag.Parse()
 	for i := 0; i < b.N; i++ {
-		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", *target))
 		if err != nil {
 			b.Error(err)
 		}
